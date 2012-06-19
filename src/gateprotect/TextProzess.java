fa40feeece58bb504a3d4a7ce70c564f7b7d2458
Daniel Villacres <undertango@gmail.com>
2012-06-19 17:56:22 +0300

    Update master

--- src/gateprotect/TextProzess.java
+++ src/gateprotect/TextProzess.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 
 // Nummern Funktionen
 import gateprotect.Nummern;
-// Betreiber Funktionen
-//import gateprotect.Betreiber;
 
 /**
  * Servlet implementation class TextProzess
@@ -27,160 +25,234 @@ public class TextProzess extends HttpServlet {
 		
 		// textarea content
 		String txtBerechnung = request.getParameter("berechnung");
+		
 		// each literal from the textarea
 		String[] worte = txtBerechnung.split("\\s+");
 		
+		// number of members
+		int mn = worte.length;
+		
+		// operations members
+		String member[] = new String[worte.length];
+		
+		// numbers values
+		int numval[] = new int[worte.length];
+		
 		// found switch
 		boolean found;
-		int nNum; // nummer
-		char sOper; // betreiber
-		String strNum; // number string
 		
-		// math operators :: add divided by -> geteilt durch
-		String[] txtbetreiber = {"plus","minus","mal"};
-		char[] betrieber = {'+','-','*'};
+		// Number string
+		String strNum;
 		
-		// equivalent numbers from literals
-		int numop[] = new int[worte.length];
+		// Result
+		float nResult = 0;
 		
-		// ocurrencies counter
-		int nn = 0;
-		// string operator
-		String LastOperator = "NEIN";
-		// here goes the result
-		int nResult = 0;
-		
+		// page content
 		PrintWriter out=response.getWriter();
 		StringBuffer sb = new StringBuffer();
 		sb.append("<html><head><title>Folge</title></head>");
 		sb.append("<body><h2>Folge</h2>");
 		sb.append("<p>Betrieb: ");
-		sb.append("<p>");
 		
-		for (String wort : worte) {
+		// search for operators and millions
+		for (int i = 0; i < mn; i++) {
 			
-			found = false;
-			Nummern num = new Nummern(); // instance number
-			//Betreiber oper = new Betreiber(); // instance operator
-			
-			// ask if is an operator
-			if(wort.equals("plus") || wort.equals("minus") || wort.equals("mal")) {
-				LastOperator = wort;
-				if(wort.equals("plus")) sb.append("+");
-				if(wort.equals("minus")) sb.append("-");
-				if(wort.equals("mal")) sb.append("*");
-				continue;
-			}
-			
-			/*
-			// search for an operator
-			sOper = oper.erhalten_betreiber(wort);
-			if((char)sOper != 'N') {
-				// operator found
-				numop[nn] = sOper;
-				found = true;
-				sb.append((char)numop[nn]);
-				nn++;
-				continue;
-			}
-			*/
+			member[i] = "nummer"; // by default
 						
-			// number 0
-			if(!found){
-				if(wort.equals("null")) {
-					numop[nn] = 0;
-					found = true;
-				}
-			} // end number 0
-			
-			// numbers 1-12
-			if(!found) {				
-				nNum = num.erhalten_1_12(wort);
-				if(nNum != 0) {
-					numop[nn] = nNum;
-					found = true;
-				}
-			} // end numbers 1-12
-			
-			// numbers from 13 to 19
-			if(!found) {
-				nNum = num.erhalten_13_19(wort);
-				if(nNum != 0) {
-					numop[nn] = nNum;
-					found = true;					
-				}
-			} // end numbers 13-19
-			
-			// numbers 20-29
-			if(!found) {
-				nNum = num.erhalten_20_29(wort);
-				if(nNum != 0) {
-					numop[nn] = nNum;
-					found = true;					
-				}				
-			} // end numbers 20-29
-			
-			// numbers 30-39
-			if(!found) {
-				nNum = num.erhalten_30_39(wort);
-				if(nNum != 0) {
-					numop[nn] = nNum;
-					found = true;					
-				}				
-			} // end numbers 30-39
-			
-			// numbers from 40 to 99
-			if(!found) {
-				nNum = num.erhalten_40_99(wort);
-				if(nNum != 0) {
-					numop[nn] = nNum;
-					found = true;					
-				}				
-			} // found 40-99
-			
-			// numbers 100-900
-			if(!found) {
-				nNum = num.erhalten_100_999(wort);
-				if(nNum != 0) {
-					numop[nn] = nNum;
-					found = true;
-				}				
-			} // end numbers 100-900
-			
-			// numbers 1000-900000
-			if(!found) {
-				nNum = num.erhalten_1000_999999(wort);
-				if(nNum != 0) {
-					numop[nn] = nNum;
-					found = true;
-				}				
-			} // end numbers 1000-900000
-			
-			// NUMBER WAS FOUND!
-			if(found) {
-				sb.append(numop[nn]);
-				nn++;
-											
-				if(LastOperator.equals("plus")) {
-					nResult = nResult + numop[nn];
-				}
-				if(LastOperator.equals("minus")) {
-					nResult = nResult - numop[nn];
-				}
-				if(LastOperator.equals("mal")) {
-					nResult = nResult * numop[nn];
-				}
+			// ask for operator
+			if(worte[i].equals("plus") || worte[i].equals("+")) {
+				member[i] = "+";
+				continue;
 			}
 			
-		} // end for worte array
+			if(worte[i].equals("minus") || worte[i].equals("-")) {
+				member[i] = "-";
+				continue;
+			}
+			
+			if(worte[i].equals("mal") || worte[i].equals("*")) {
+				member[i] = "*";
+				continue;
+			}
+			
+			if(worte[i].equals("geteilt")) {
+				//member[i] = "/";
+				member[i] = "";
+				continue;
+			}
+			
+			if(worte[i].equals("durch") || worte[i].equals("*")) {
+				member[i] = "/";
+				continue;
+			}
+			
+			if(worte[i].indexOf("millionen") == 0) {
+				member[i] = "millionen";
+			}
+			
+		} // end for search operators
 		
-		// operation result
+		// convert numbers from strings
+		for (int i = 0; i < mn; i++) {
+			
+			// check if is a number
+			if(member[i] == "nummer") {
+				
+				found = false;
+				strNum = worte[i];
+				Nummern num = new Nummern(); // instance number
+				
+				// check if actually is a number
+				numval[i] = num.tryParse(strNum);
+				
+				// it's a literal.
+				if(numval[i] == 0) {
+				
+				// check if a number of millions is (yoda be proud of me!) xD
+				if( (i < (mn-1)) && (member[i+1] == "millionen") ) {
+					
+					// MILLIONS -> 1.000.000 - 9.999.999
+					numval[i] = num.erhalten_1000000_9999999(strNum, worte[i+1]);
+					if(numval[i] != 0) {
+						found = true;
+					} // end numbers 1000-999999
+					
+				}
+								
+				// number 0
+				if(!found){
+					if(strNum.equals("null")) {
+						numval[i] = 0;
+						found = true;
+					}
+				} // end number 0
+				
+				// numbers 1-12
+				if(!found) {				
+					numval[i] = num.erhalten_1_12(strNum);
+					if(numval[i] != 0) {
+						found = true;
+					}
+				} // end numbers 1-12
+				
+				// numbers 13-19
+				if(!found) {			
+					numval[i] = num.erhalten_13_19(strNum);
+					if(numval[i] != 0) {
+						found = true;
+					}
+				} // end numbers 13-19
+				
+				// numbers 20-29
+				if(!found) {				
+					numval[i] = num.erhalten_20_29(strNum);
+					if(numval[i] != 0) {
+						found = true;
+					}
+				} // end numbers 20-29
+				
+				// numbers 30-39
+				if(!found) {				
+					numval[i] = num.erhalten_30_39(strNum);
+					if(numval[i] != 0) {
+						found = true;
+					}
+				} // end numbers 30-39
+				
+				// numbers 40-99
+				if(!found) {				
+					numval[i] = num.erhalten_40_99(strNum);
+					if(numval[i] != 0) {
+						found = true;
+					}
+				} // end numbers 40-99
+				
+				// numbers 100-999
+				if(!found) {				
+					numval[i] = num.erhalten_100_999(strNum);
+					if(numval[i] != 0) {
+						found = true;
+					}
+				} // end numbers 100-999
+				
+				// numbers 1000-999999
+				if(!found) {				
+					numval[i] = num.erhalten_1000_999999(strNum);
+					if(numval[i] != 0) {
+						found = true;
+					}
+				} // end numbers 1000-999999
+				
+				} // if(numval[i] == 0)
+				
+			} // end check number
+			
+		} // end convert numbers from strings
+		
+		// show operation
+		for( int i = 0; i < mn; i++ ) {
+			
+			// ask for data type
+			if(member[i].equals("nummer")) {
+				// nummern
+				sb.append(numval[i]);
+			} else {
+				// operators
+				if( member[i] != "million" && member[i] != "millionen" ) {
+					sb.append(member[i]);
+				}				
+			}			
+		} // end show operation
+		
+		// calculate
+		for( int i = 0; i < mn; i++ ) {
+			
+			// ask if the first number is
+			if( nResult == 0 ) {
+				
+				// it's the first number
+				if( member[0] == "nummer") {
+					nResult = numval[i];
+				}
+				// first number is negative
+				if( (member[0] == "-") && (member[1] == "nummer") ) {
+					nResult = numval[i] * -1;
+				}
+				
+			} else {
+				
+				// nummer ?
+				if( member[i] == "nummer" ) {
+					
+					// plus
+					if( member[i-1] == "+" ) {
+						nResult = nResult + numval[i];
+					}
+					// minus
+					if( member[i-1] == "-" ) {
+						nResult = nResult - numval[i];
+					}
+					// mal
+					if( member[i-1] == "*" ) {
+						nResult = nResult * numval[i];
+					}
+					// geteilt durch -> // (double) :: numval > 0 to avoid divided by zero error
+					if( (member[i-1] == "/") && (member[i-2] == "") && (numval[i] > 0)) {
+						nResult = nResult / numval[i];
+					}
+				}
+				
+			}					
+			
+		} // end calculate
+		
 		sb.append("<p>Result: ");
 		sb.append(nResult);
 		sb.append("<p><a href=rechner>Back</a>");
 		sb.append("</body></html>");
 		out.println(sb);
 		out.close();
+		
 	}
 
 	/**
